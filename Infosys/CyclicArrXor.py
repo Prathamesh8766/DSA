dd35d286702012c96147a0a48d86952d88de2367
Prathamesh <prathampatil123789@gmail.com>
2026-05-05 23:41:33 +0530

    Solved infosys previous year  Problem

--- Infosys/CyclicArrXor.py
+++ Infosys/CyclicArrXor.py
@@ -31,4 +31,37 @@ def cyclicArr():
             max_val = sum
     return max_val
             
-print(cyclicArr())
+# print(cyclicArr())
+
+def arrayElementRemovel():
+    arr = list(map(int, input().split(",")))
+    steps = 0
+
+    arr.sort()   # ✅ fixed
+
+    if len(arr) == 0:
+        return 0
+    if len(arr) == 1:
+        return 1
+
+    i = 0
+    j = len(arr) - 1
+
+    while i <= j:  
+        if arr[i] != arr[j]:
+            steps += 1
+        else:
+            break
+
+        i += 1
+        j -= 1   
+
+    if i == j:
+        return steps + 1
+    elif i < j:
+        return steps + (j - i + 1)
+    else:
+        return steps
+
+
+print(arrayElementRemovel())
