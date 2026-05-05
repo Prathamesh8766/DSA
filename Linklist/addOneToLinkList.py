8884f19c19f3210822545ce354ac1f6c4d9b3061
Prathamesh <prathampatil123789@gmail.com>
2026-05-05 19:05:54 +0530

    Solved add One to the LinkLis using backTracking

--- Linklist/addOneToLinkList.py
+++ Linklist/addOneToLinkList.py
@@ -39,8 +39,30 @@ def addOne(head):
     head = reverse(head)
     return head
 
+def BackTracking(head):
+    def helper(temp):
+        if temp is None: return 1
+        carry = helper(temp.next)
+        temp.data = temp.data + carry
+        if temp.data < 10: return 0
+        temp.data = 0
+        return 1
+    carry = helper(head)
+    if carry != 0:
+        newNode = Node(data = 1)
+        newNode.next = head
+        head = newNode
+    return head
+
 l3= LinkList()
 l3.createLinkList()
 l3.displayLinkList()
-l3.head = addOne(l3.head)
+l3.head = BackTracking(l3.head)
+
 l3.displayLinkList()
+l4= LinkList()
+l4.createLinkList()
+l4.displayLinkList()
+l4.head = BackTracking(l4.head)
+
+l4.displayLinkList()
