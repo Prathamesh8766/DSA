d2d3c6cc6079cc5f15407b27a7878b1a0db5fe91
Prathamesh <prathampatil123789@gmail.com>
2026-05-26 15:49:21 +0530

    Solved bipartite in graph

---
+++ Graph/bipartite.py
@@ -0,0 +1,21 @@
+def bipartite(graph):
+
+    adj = {}
+
+    for i in range(len(graph)):
+
+        adj[i] = graph[i]
+    visited = [0]*len(adj)
+    def dfs(node,color):
+
+        visited[node] = color
+        for negebhour in  adj[node]:
+            if visited[negebhour] ==0:
+                if dfs(negebhour,-color) == False: return False
+            elif visited[negebhour] == color: return False
+        return True
+    for i in range(len(adj)):
+        if visited[i] == 0:
+            if dfs(i,1) == False: return False
+    return True
+print(bipartite([[1,2,3],[0,2],[0,1,3],[0,2]]))
